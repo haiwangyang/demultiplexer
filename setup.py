505e556acb337b4103c35f7886ca0c90c1318bb3
haiwangyang <haiwangyang@gmail.com>
2018-03-12 17:04:18 -0400

    m

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='demultiplexer',
+      version='1.0',
+      description='demultiplex bam with mixed species into specie-level sam',
+      url='http://github.com/haiwangyang/demultiplexer',
+      author='Haiwang Yang',
+      author_email='haiwangyang@gmail.com',
+      license='MIT',
+      packages=['demultiplexer'],
+      zip_safe=False)
